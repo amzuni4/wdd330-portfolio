341ecf2bd0ee80f110ecc12accf412c99df92bb2
Amy Zuniga <amzuni@gmail.com>
2020-09-22 18:03:02 -0700

    Commited week 2 Reading Code Examples

---
+++ week2/SitePoint/chapter2/main.js
@@ -0,0 +1,10 @@
+
+//This declares a variable called question and assigns the string
+// 'What is Superman's real name?' to it.
+const question = "What is Superman's real name?"
+//Next, we need to ask the question stored in the question variable, using a prompt dialog:
+const answer = prompt(question);
+//A prompt dialog allows the player to type in a response, 
+//which is then stored in the variable it is assigned to, which is answer in this case.
+alert(`You answered ${answer}`);
+
