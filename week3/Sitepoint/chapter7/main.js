38b9769c8aea761e50277fce334ea19bcb482124
Amy Zuniga <amzuni@gmail.com>
2020-10-02 16:24:42 -0700

    Chapter 7 Corrections

--- week3/Sitepoint/chapter7/main.js
+++ week3/Sitepoint/chapter7/main.js
@@ -1,41 +1,41 @@
 const quiz = [
     { name: "Superman", realName: "Clark Kent" },
-    { name: "Wonder Woman", realName: "Diana Prince" },
+    { name: "Wonderwoman", realName: "Dianna Prince" },
     { name: "Batman", realName: "Bruce Wayne" },
 ];
 
 // View Object
 const view = {
-    show(element) {
-        element.style.display = 'block';
-    },
-    hide(element) {
-        element.style.display = 'none';
-    },
-  start: document.getElementById('start'),
     score: document.querySelector('#score strong'),
     question: document.getElementById('question'),
     result: document.getElementById('result'),
     info: document.getElementById('info'),
+    start: document.getElementById('start'),
     render(target, content, attributes) {
         for (const key in attributes) {
             target.setAttribute(key, attributes[key]);
         }
         target.innerHTML = content;
+    },
+    show(element) {
+        element.style.display = 'block';
+    },
+    hide(element) {
+        element.style.display = 'none';
     }
-
 };
 
+// Game Object
 const game = {
     start(quiz) {
-        this.questions = [...quiz];
         this.score = 0;
+        this.questions = [...quiz];
+        view.hide(view.start);
         // main game loop
         for (const question of this.questions) {
             this.question = question;
             this.ask();
         }
-        view.hide(view.start);
         // end of main game loop
         this.gameOver();
     },
@@ -58,9 +58,9 @@ const game = {
         }
     },
     gameOver() {
-        view.show(view.start);
         view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
-        
+        view.show(view.start);
     }
 }
+
 view.start.addEventListener('click', () => game.start(quiz), false);
