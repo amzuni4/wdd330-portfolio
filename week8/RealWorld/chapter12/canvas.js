fde9604ad4a74167bb16f28f0933ccacda6bf50f
Amy Zuniga <amzuni@gmail.com>
2021-02-23 12:23:35 -0800

    Week 8-10 up

---
+++ week8/RealWorld/chapter12/canvas.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById("myCanvas"); 
+
+context.strokeStyle = "red";
+context.fillStyle = "rgba(0, 0, 255, 0.5)";
+context.fillRect(10, 10, 100, 100);   
+context.strokeRect(10, 10, 100, 100);
+
+
+function drawPattern() {
+    var canvas = document.getElementById("demo2");
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "red";
+    
+    var img = new Image();
+    img.src = "./images/bg-bike.png";
+    img.onload = function() { 
+    };            
+}
